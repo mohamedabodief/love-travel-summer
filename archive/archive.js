7c44c3ba6bc73574aa657d92d438834243249112
mohamedabodief <mohamed.abodeaf@gmail.com>
2025-05-07 11:37:26 +0300

    Initial commit

---
+++ archive/archive.js
@@ -0,0 +1,33 @@
+
+//swiper.js
+
+//carousel-section
+
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    spaceBetween: 30,
+    slidesPerView: 3,
+    loop: true,
+
+    pagination: {
+        el: '.swiper-pagination',
+    },
+
+    autoplay: {
+        delay: 1500,
+        disableOnInteraction: false,
+    },
+
+
+});
+
+
+
+
+// --------------------------------------------------------------------------------------------------------------------------
+
+
+
+
+
+
